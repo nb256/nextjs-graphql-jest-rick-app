ae2a9bfc775d57a43117472f90264ad0d907590d
Naim Buru <naim.buru@hemenis.com>
2021-07-02 02:25:48 +0300

    add countResidentStats function

---
+++ functions/countResidentStats.ts
@@ -0,0 +1,40 @@
+import { Location } from "../types"
+
+export default ({ location }: { location: Location }) => {
+    let deadCount = 0;
+    let aliveCount = 0;
+    let guestCount = 0;
+    let robotCount = 0;
+    let alienCount = 0;
+    let humanCount = 0;
+
+    location.residents.forEach(resident => {
+        if (resident.status === "Alive") {
+            aliveCount++
+        } else if (resident.status === "Dead") {
+            deadCount++
+        }
+
+        if (resident.origin.id !== location.id) {
+            guestCount++
+        }
+
+        if (resident.species === "Human") {
+            humanCount++
+        } else if (resident.species === "Robot") {
+            robotCount++
+        } else if (resident.species === "Alien") {
+            alienCount++
+        }
+    })
+
+    return {
+        deadCount,
+        aliveCount,
+        guestCount,
+        robotCount,
+        alienCount,
+        humanCount
+    }
+
+}
