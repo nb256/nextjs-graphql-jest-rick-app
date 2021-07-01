596816f0aa4882058f5aa0b1fb0a6e18de463b69
Naim Buru <naim.buru@hemenis.com>
2021-07-02 00:03:34 +0300

    add getLocations function

---
+++ functions/getLocations.ts
@@ -0,0 +1,31 @@
+import client from "../apollo-client";
+import { gql } from "@apollo/client";
+
+const getLocations = async ({ page, filter }: { page: number, filter?: { type: string, dimension: string } }) => {
+  try {
+    const { data } = await client.query({
+      query: gql`
+            query GetLocations($page: Int, $filter: FilterLocation) {
+                locations (page: $page, filter: $filter) {
+                  info {
+                    count
+                    pages
+                  }
+                  results {
+                    id
+                    name
+                    type
+                    dimension
+                  }
+                }
+              }
+        `,
+      variables: { page, filter }
+    });
+    return data
+
+  } catch (error) {
+    console.log(JSON.stringify(error))
+  }
+}
+export default getLocations
